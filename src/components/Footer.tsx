e783de7a9be7135adc63168db2accd58c1a09d43
dohyeons <ddhhss0603@gmail.com>
2025-03-19 20:37:33 +0900

    feat: 기본적인 헤더 및 푸터 레이아웃 구현

---
+++ src/components/Footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <footer className="bg-footer-gray h-[57px]">최하단 푸터</footer>;
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <header className="p-2 fixed top-0 left-0 right-0 bg-header-pink">헤더</header>;
+}
