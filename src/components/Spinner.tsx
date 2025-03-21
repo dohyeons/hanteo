15428bee7a51e6a6d7814d4803d217fa11067683
dohyeons <ddhhss0603@gmail.com>
2025-03-21 11:13:39 +0900

    feat: 로딩 스피너 구현

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,7 @@
+export default function Spinner() {
+  return (
+    <div className="flex w-full justify-center">
+      <span className="size-5 animate-spin rounded-full border-2 border-t-transparent border-gray-400" />
+    </div>
+  );
+}
