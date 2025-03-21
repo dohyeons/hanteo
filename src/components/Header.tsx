307176e3e6c9abeb0bcd8e9e31a00f582f02badf
dohyeons <ddhhss0603@gmail.com>
2025-03-21 14:22:17 +0900

    feat: 헤더 z-index 설정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,7 +13,7 @@ const NAV_ITEMS = [
 
 export default function Header() {
   return (
-    <header className="fixed top-0 left-0 flex items-center px-2 right-0 bg-header-pink h-9">
+    <header className="fixed z-50 top-0 left-0 flex items-center px-2 right-0 bg-header-pink h-9">
       <DragScrollContainer>
         <ul className="w-full min-w-[450px] gap-4 max-w-[1200px] h-full flex mx-auto">
           {NAV_ITEMS.map((item) => (
