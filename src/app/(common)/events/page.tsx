bde19c7f856b92bab31355d7042cf5c4551dd258
dohyeons <ddhhss0603@gmail.com>
2025-03-21 14:17:46 +0900

    feat: ContentList 적용

--- src/app/(common)/events/page.tsx
+++ src/app/(common)/events/page.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import { useSuspenseInfiniteQuery } from "@tanstack/react-query";
+import ContentList from "@/components/ContentList";
 import axios from "axios";
 import Image from "next/image";
-import { useEffect } from "react";
-import { useInView } from "react-intersection-observer";
 
 type Post = {
   id: number;
@@ -26,50 +24,27 @@ const fetchPosts = async ({
 };
 
 export default function Page() {
-  const { data, error, isFetchingNextPage, hasNextPage, fetchNextPage } = useSuspenseInfiniteQuery({
-    queryKey: ["event"],
-    queryFn: fetchPosts,
-    getNextPageParam: (lastPage) => {
-      return lastPage.nextPage;
-    },
-    initialPageParam: 1,
-  });
-  const { ref, inView } = useInView({
-    threshold: 0.5,
-    triggerOnce: false,
-  });
-  useEffect(() => {
-    if (inView && hasNextPage && !isFetchingNextPage) {
-      fetchNextPage();
-    }
-  }, [inView, isFetchingNextPage, hasNextPage]);
-
-  if (error) {
-    throw new Error("목록 조회 중 에러가 발생했습니다.");
-  }
-
   return (
-    <>
-      {data?.pages.map((page) =>
-        page.data.map((item) => (
-          <div key={item.id} className="bg-white rounded-lg flex gap-4 w-full">
-            <Image
-              src={"/default_image.jpg"}
-              alt={"순위 이미지"}
-              width={1280}
-              height={850}
-              className="max-w-[100px] md:max-w-[200px] w-full rounded-lg"
-            />
-            <div className="flex flex-col">
-              <h3 className="text-lg font-bold line-clamp-1 overflow-hidden text-ellipsis">
-                {item.title}
-              </h3>
-              <p className="line-clamp-2 overflow-hidden text-ellipsis">{item.body}</p>
-            </div>
+    <ContentList
+      queryKey={["event"]}
+      fetchFunction={fetchPosts}
+      renderItem={(item) => (
+        <div key={item.id} className="bg-white rounded-lg flex gap-4 w-full shadow-card">
+          <Image
+            src={"/default_image.jpg"}
+            alt={"순위 이미지"}
+            width={1280}
+            height={850}
+            className="max-w-[100px] md:max-w-[200px] w-full rounded-lg"
+          />
+          <div className="flex flex-col">
+            <h3 className="text-lg font-bold line-clamp-1 overflow-hidden text-ellipsis">
+              {item.title}
+            </h3>
+            <p className="line-clamp-2 overflow-hidden text-ellipsis">{item.body}</p>
           </div>
-        )),
+        </div>
       )}
-      <div ref={ref}>{isFetchingNextPage ? "Loading more..." : " "}</div>
-    </>
+    />
   );
 }
