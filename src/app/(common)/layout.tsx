8dfbad058b9ba45d512a5bf85401732b5dd014ae
dohyeons <ddhhss0603@gmail.com>
2025-03-21 11:14:09 +0900

    feat: Suspense를 사용한 로딩 관리 구현

--- src/app/(common)/layout.tsx
+++ src/app/(common)/layout.tsx
@@ -1,10 +1,15 @@
 import ContentHeader from "@/app/(common)/_components/ContentHeader";
+import Spinner from "@/components/Spinner";
+import { Suspense } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="p-4 flex flex-col gap-2">
+    <div className="p-4 flex flex-col flex-1 gap-2">
       <ContentHeader />
-      <div className="flex flex-col gap-4 w-full">{children}</div>
+
+      <div className="flex flex-1 flex-col gap-4 w-full items-center justify-center">
+        <Suspense fallback={<Spinner />}>{children} </Suspense>
+      </div>
     </div>
   );
 }
