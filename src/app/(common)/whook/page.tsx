e7d4634e0e71bf65434a53375499fe778ab10c72
dohyeons <ddhhss0603@gmail.com>
2025-03-22 17:05:10 +0900

    feat: fetchPosts 함수 분리

--- src/app/(common)/whook/page.tsx
+++ src/app/(common)/whook/page.tsx
@@ -2,26 +2,7 @@
 
 import ContentCard from "@/components/ContentCard";
 import ContentList from "@/components/ContentList";
-import axios from "axios";
-
-type Post = {
-  id: number;
-  title: string;
-  body: string;
-  userId: number;
-};
-const fetchPosts = async ({
-  pageParam = 1,
-}): Promise<{ data: Post[]; nextPage: number | null }> => {
-  try {
-    const res = await axios.get(
-      `https://jsonplaceholder.typicode.com/posts?_page=${pageParam}&_limit=10`,
-    );
-    return { data: res.data, nextPage: res.data.length < 10 ? null : pageParam + 1 };
-  } catch {
-    throw new Error("Failed to fetch posts");
-  }
-};
+import fetchPosts from "@/lib/\bapi/fetchPosts";
 
 export default function Page() {
   return (
