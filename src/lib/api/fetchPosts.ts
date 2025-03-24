93aa67147237e4597c213e1394d5973061bf78fa
dohyeons <ddhhss0603@gmail.com>
2025-03-24 09:53:58 +0900

    feat: axiosInstance 설정

--- src/lib/api/fetchPosts.ts
+++ src/lib/api/fetchPosts.ts
@@ -1,13 +1,11 @@
+import axiosInstance from "@/lib/axiosInstance";
 import { Post } from "@/types";
-import axios from "axios";
 
 const fetchPosts = async ({
   pageParam = 1,
 }): Promise<{ data: Post[]; nextPage: number | null }> => {
   try {
-    const res = await axios.get(
-      `https://jsonplaceholder.typicode.com/posts?_page=${pageParam}&_limit=10`,
-    );
+    const res = await axiosInstance.get(`posts?_page=${pageParam}&_limit=10`);
     return { data: res.data, nextPage: res.data.length < 10 ? null : pageParam + 1 };
   } catch {
     throw new Error("Failed to fetch posts");
